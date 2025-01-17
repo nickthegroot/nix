f28875d8c9b4f616aa9ecd7294a848b903e26f87
Nick DeGroot <nick@nickthegroot.com>
2025-01-17 11:31:38 -0800

    tmux: include tmuxinator & fzf plugin

--- home/base/tui/tmux.nix
+++ home/base/tui/tmux.nix
@@ -2,23 +2,10 @@
   pkgs,
   lib,
   ...
-}: let
-  vim-tmux-navigation =
-    pkgs.tmuxPlugins.mkTmuxPlugin
-    {
-      pluginName = "vim-tmux-navigation";
-      version = "2024-12-06";
-      src = pkgs.fetchFromGitHub {
-        owner = "christoomey";
-        repo = "vim-tmux-navigator";
-        rev = "d847ea942a5bb4d4fab6efebc9f30d787fd96e65";
-        sha256 = "EkuAlK7RSmyrRk3RKhyuhqKtrrEVJkkuOIPmzLHw2/0=";
-      };
-      rtpFilePath = "vim-tmux-navigator.tmux";
-    };
-in {
+}: {
   programs.tmux = {
     enable = true;
+    tmuxinator.enable = true;
     keyMode = "vi";
     mouse = true;
     prefix = "C-a";
@@ -32,11 +19,7 @@ in {
       bind  %  split-window -h -c "#{pane_current_path}"
       bind '"' split-window -v -c "#{pane_current_path}"
     '';
-    plugins = [
-      {
-        plugin = vim-tmux-navigation;
-      }
-    ] ++ (with pkgs.tmuxPlugins; [
+    plugins = with pkgs.tmuxPlugins; [
         {
           plugin = cpu;
           extraConfig = ''
@@ -57,8 +40,10 @@ in {
             set -agF status-right "#{E:@catppuccin_status_weather}"
           '';
         }
+        tmux-fzf
         fzf-tmux-url
-    ]);
+        vim-tmux-navigator
+    ];
   };
 
   catppuccin.tmux.extraConfig = ''

a9998439635f9d8fe4c4d5615c1a03db94e1e1c5
Nick DeGroot <nick@nickthegroot.com>
2025-01-21 18:26:57 -0800

    yabai animations

--- modules/darwin/window-manager.nix
+++ modules/darwin/window-manager.nix
@@ -1,31 +1,31 @@
 {
-  # Note: should be kept in sync with home/linux/gui/hyprland/conf/hyprland.conf
-  
   services.yabai = {
     enable = true;
     enableScriptingAddition = true;
     config = {
       mouse_follows_focus = "on";
       window_placement = "second_child";
-      window_topmost = "off";
-      window_opacity = "on";
-      window_border = "off";
-      window_shadow = "on";
-      active_window_border_color = "0xff008bff";
-      normal_window_border_color = "0x01000000";
-      active_window_opacity = "1.0";
-      normal_window_opacity = "0.94" ;
-      split_ratio = "0.50";
+      window_shadow = "float";
+      active_window_opacity = 1.0;
+      normal_window_opacity = 0.9;
+      split_ratio = 0.5;
       auto_balance = "off";
-      window_animation_duration = "0.0";
+
+      # Animation
+      window_animation_duration = 0.5;
+      window_animation_easing = "ease_out_quint";
+      window_opacity_duration = 0.2;
+      
+      # Opacity
+      window_opacity = "on";
 
       # general space settings
       layout = "bsp";
-      top_padding = "5";
-      bottom_padding = "5";
-      left_padding = "5";
-      right_padding = "5";
-      window_gap = "10";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 10;
       external_bar = "all:32:0";
     };
     

cb23a75066222be01cf8089328e3b135d71efc86
Nick DeGroot <nick@nickthegroot.com>
2025-02-03 10:06:54 -0800

    fix macos ssh config

--- home/base/tui/ssh.nix
+++ home/base/tui/ssh.nix
@@ -2,7 +2,7 @@
   opPath =
     if pkgs.stdenv.isLinux
     then "~/.1password/agent.sock"
-    else "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    else "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
 in {
   programs.ssh = {
     enable = true;

7056f1d4bc219a30e0aef14897331c0b11ada25a
Nick DeGroot <nick@nickthegroot.com>
2025-02-03 10:36:19 -0800

    vim: hide read notifications

--- home/base/tui/neovim/plugins/ui/dashboard.nix
+++ home/base/tui/neovim/plugins/ui/dashboard.nix
@@ -29,7 +29,7 @@ in {
                 local cmds = {
                   {
                     title = "Notifications",
-                    cmd = "gh notify -s -a -n5",
+                    cmd = "gh notify -s -n5",
                     action = function()
                       vim.ui.open("https://github.com/notifications")
                     end,

640d1d3b2976f90decd0c5bfc7473ad41145056c
Nick DeGroot <nick@nickthegroot.com>
2025-03-02 20:33:14 -0800

    nixos: japanese input

---
+++ home/linux/gui/hyprland/language-input.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  catppuccin.fcitx5.enable = true;
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-configtool
+      fcitx5-mozc
+      fcitx5-gtk # gtk im module
+    ];
+  };
+}

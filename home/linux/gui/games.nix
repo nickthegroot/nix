e2af4d18351880d801c3f04dac47247d77efa2da
Nick DeGroot <nick@nickthegroot.com>
2025-03-15 00:08:45 -0700

    mangohud tweak

--- home/linux/gui/games.nix
+++ home/linux/gui/games.nix
@@ -10,8 +10,15 @@
   programs.mangohud = {
     enable = true;
     settings = {
-      full = true; # show (most) parameters
       no_display = true; # hide hud by default
+      # pre defined presets
+      # -1 = default
+      #  0 = no display
+      #  1 = fps only
+      #  2 = horizontal view
+      #  3 = extended
+      #  4 = high detailed information
+      preset = 3;
     };
   };
 }

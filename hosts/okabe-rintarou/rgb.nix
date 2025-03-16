cf7f122d0df184c0a4b3a16a64dd8eb98f05a74b
Nick DeGroot <nick@nickthegroot.com>
2025-03-16 00:22:11 -0700

    nixos: auto-apply rgb

--- hosts/okabe-rintarou/rgb.nix
+++ hosts/okabe-rintarou/rgb.nix
@@ -1,18 +1,15 @@
 { pkgs, ... }:
 let
-  inherit (pkgs) writeShellScript;
-
   liquidctl = "${pkgs.liquidctl}/bin/liquidctl";
-  setCaseRgb = writeShellScript "set-rgb" ''
-    ${liquidctl} --match "Aura" set sync color static 8A00C4
-    ${liquidctl} --match "Kraken" set sync color fixed 8A00C4
-    ${liquidctl} --match "Smart Device" set sync color fixed 04A5E5
-  '';
 in
 {
   systemd.services.setCaseRgb = {
     wantedBy = [ "multi-user.target" ];
     description = "Set RGB lighting on startup";
-    script = setCaseRgb;
+    script = ''
+      ${liquidctl} --match "Aura" set sync color static 8A00C4
+      ${liquidctl} --match "Kraken" set sync color fixed 8A00C4
+      ${liquidctl} --match "Smart Device" set sync color fixed 04A5E5
+    '';
   };
 }

d969b7c818b0470601054f29ef1b4bd1435062a2
Nick DeGroot <nick@nickthegroot.com>
2025-03-29 17:03:19 -0700

    NixOS: switch monitors

--- hosts/okabe-rintarou/home.nix
+++ hosts/okabe-rintarou/home.nix
@@ -2,15 +2,14 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-2,2560x1440@143.91Hz,auto,1"
-        "DP-1,3440x1440@159.96Hz,auto-left,1"
+        "DP-2,highrr,auto,1"
+        "DP-1,highrr,auto-left,1,transform,1"
+      ];
+
+      workspace = [
+        # Set DP-2 as primary (workspace 1)
+        "1,monitor:DP-2"
       ];
     };
-  };
-
-  # Widescreen monitor is on an arm, which allows me to use in vertical
-  home.shellAliases = {
-    "monitor-vert" = "hyprctl keyword monitor DP-1,3440x1440@159.96Hz,auto-left,1,transform,3";
-    "monitor-horiz" = "hyprctl keyword monitor DP-1,3440x1440@159.96Hz,auto-left,1";
   };
 }

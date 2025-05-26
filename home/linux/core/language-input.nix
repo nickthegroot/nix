d4d23a418db6e5dfca35f439c914216e976b5f32
Nick DeGroot <nick@nickthegroot.com>
2025-05-26 13:05:41 -0700

    nixos: update i18n config

--- home/linux/core/language-input.nix
+++ home/linux/core/language-input.nix
@@ -3,7 +3,8 @@
   catppuccin.fcitx5.enable = true;
 
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-configtool
       fcitx5-mozc

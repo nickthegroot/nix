a98c77168de8c945ee8d8a38109e7720a7151f30
Nick DeGroot <nick@nickthegroot.com>
2025-06-03 13:43:14 -0700

    init: newsboat

---
+++ home/base/tui/newsboat.nix
@@ -0,0 +1,44 @@
+{
+  programs.newsboat = {
+    enable = true;
+
+    autoReload = true;
+    urls = [
+      {
+        url = "https://news.ycombinator.com/rss";
+        title = "Hacker News";
+      }
+      {
+        url = "https://feeds.npr.org/1002/rss.xml";
+        title = "NPR Top Stories";
+      }
+    ];
+
+    # https://m-chrzan.xyz/blog/vim-keybindings-for-newsboat.html
+    extraConfig = ''
+      # jk - move up/down
+      unbind-key j
+      unbind-key k
+      bind-key j down
+      bind-key k up
+
+      # hl - move in/out of feeds/articles
+      bind-key h quit
+      bind-key l open
+
+      # q quits the program
+      unbind-key q
+      bind-key q hard-quit
+
+      # g/G to go to top/bottom
+      unbind-key g
+      unbind-key G
+      bind-key g home
+      bind-key G end
+
+      # f to follow links (like qutebrowser)
+      unbind-key f
+      bind-key f goto-url
+    '';
+  };
+}

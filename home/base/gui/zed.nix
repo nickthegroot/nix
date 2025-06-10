63ecc4ab786d5c728c341b2274aad2c11c2ac735
Nick DeGroot <nick@nickthegroot.com>
2025-06-10 14:02:52 -0700

    zed: configure core keybinds / settings

--- home/base/gui/zed.nix
+++ home/base/gui/zed.nix
@@ -1,12 +1,36 @@
 {
   programs.zed-editor = {
     enable = true;
+
     userSettings = {
-      vim_mode = true;
+      on_last_window_closed = "quit_app";
+
+      ui_font_family = "JetBrainsMono Nerd Font";
+      buffer_font_family = "JetBrainsMono Nerd Font";
+
       relative_line_numbers = true;
+      vim_mode = true;
       vim = {
         use_system_clipboard = "on_yank";
       };
     };
+
+    userKeymaps = [
+      {
+        bindings = {
+          ctrl-h = "workspace::ActivatePaneLeft";
+          ctrl-l = "workspace::ActivatePaneRight";
+          ctrl-k = "workspace::ActivatePaneUp";
+          ctrl-j = "workspace::ActivatePaneDown";
+        };
+      }
+
+      {
+        context = "VimControl && !menu";
+        bindings = {
+          "space space" = "file_finder::Toggle";
+        };
+      }
+    ];
   };
 }

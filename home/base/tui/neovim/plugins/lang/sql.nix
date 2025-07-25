0a45ac6a836344932df58a84108ed895d767319c
Nick DeGroot <nick@nickthegroot.com>
2025-07-25 12:31:02 -0700

    vim: sql lsp

---
+++ home/base/tui/neovim/plugins/lang/sql.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+{
+  programs.nixvim = {
+    plugins = {
+      conform-nvim.settings = {
+        formatters_by_ft.sql = [ "sqlformatter" ];
+        formatters.sqlformatter.command = "${pkgs.sql-formatter}/bin/sql-formatter";
+      };
+
+      lsp.servers.postgres_lsp.enable = true;
+    };
+  };
+}

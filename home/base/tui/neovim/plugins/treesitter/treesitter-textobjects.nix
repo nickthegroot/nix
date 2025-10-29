173926c83dca7765e0bc0f3b8bf94e6e06bc4d1d
Nick DeGroot <nick@nickthegroot.com>
2025-10-28 21:01:38 -0700

    nix: update flakes

--- home/base/tui/neovim/plugins/treesitter/treesitter-textobjects.nix
+++ home/base/tui/neovim/plugins/treesitter/treesitter-textobjects.nix
@@ -2,38 +2,40 @@
   programs.nixvim = {
     plugins.treesitter-textobjects = {
       enable = true;
-      select = {
-        enable = true;
-        lookahead = true;
-        keymaps = {
-          "aa" = "@assignment.outer";
-          "ia" = "@assignment.inner";
-          "ab" = "@block.outer";
-          "ib" = "@block.inner";
-          "af" = "@function.outer";
-          "if" = "@function.inner";
-          "aC" = "@class.outer";
-          "iC" = "@class.inner";
-          "at" = "@comment.outer";
+      settings = {
+        select = {
+          enable = true;
+          lookahead = true;
+          keymaps = {
+            "aa" = "@assignment.outer";
+            "ia" = "@assignment.inner";
+            "ab" = "@block.outer";
+            "ib" = "@block.inner";
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "aC" = "@class.outer";
+            "iC" = "@class.inner";
+            "at" = "@comment.outer";
+          };
         };
-      };
-      move = {
-        enable = true;
-        gotoNextStart = {
-          "]f" = "@function.outer";
-          "]b" = "@block.outer";
-        };
-        gotoNextEnd = {
-          "]F" = "@function.outer";
-          "]B" = "@block.outer";
-        };
-        gotoPreviousStart = {
-          "[f" = "@function.outer";
-          "[b" = "@block.outer";
-        };
-        gotoPreviousEnd = {
-          "[F" = "@function.outer";
-          "[B" = "@block.outer";
+        move = {
+          enable = true;
+          goto_next_start = {
+            "]f" = "@function.outer";
+            "]b" = "@block.outer";
+          };
+          goto_next_end = {
+            "]F" = "@function.outer";
+            "]B" = "@block.outer";
+          };
+          goto_previous_start = {
+            "[f" = "@function.outer";
+            "[b" = "@block.outer";
+          };
+          goto_previous_end = {
+            "[F" = "@function.outer";
+            "[B" = "@block.outer";
+          };
         };
       };
     };

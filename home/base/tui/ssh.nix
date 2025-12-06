20ca46fa4bbb60f2a3d0e2a1868e775bcf6bf438
Nick DeGroot <nick@nickthegroot.com>
2025-12-06 15:31:25 -0800

    ssh: fix deprecated defaultConfig

--- home/base/tui/ssh.nix
+++ home/base/tui/ssh.nix
@@ -1,17 +1,21 @@
 { pkgs, ... }:
-let
-  opPath =
-    if pkgs.stdenv.isLinux then
-      "~/.1password/agent.sock"
-    else
-      "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-in
 {
   programs.ssh = {
     enable = true;
-    includes = [ "~/.ssh/hosts_config" ]; # local & remote servers
-    extraConfig = ''
-      IdentityAgent ${opPath}
-    '';
+    enableDefaultConfig = false; # deprecated
+    includes = [
+      "~/.ssh/hosts_config" # https://developer.1password.com/docs/ssh/agent/advanced#use-multiple-github-accounts
+      "~/.ssh/1Password/config" # https://developer.1password.com/docs/ssh/bookmarks/
+    ];
+
+    matchBlocks = {
+      "*" = {
+        identityAgent =
+          if pkgs.stdenv.isDarwin then
+            "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+          else
+            "~/.1password/agent.sock";
+      };
+    };
   };
 }

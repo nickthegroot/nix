219d5f60074fabc6eafa9bcd1662c1828e3f7211
Nick DeGroot <nick@nickthegroot.com>
2025-12-06 15:31:41 -0800

    dankMaterialShell: load config into greeter

--- modules/nixos/login.nix
+++ modules/nixos/login.nix
@@ -1,9 +1,23 @@
-{ dank-material-shell, ... }:
+{
+  dank-material-shell,
+  quickshell,
+  pkgs,
+  myvars,
+  ...
+}:
+let
+  # Required for idle monitoring
+  # Current nixpkgs upstream is reported as "not new enough"
+  quickshellPkg = quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
+in
 {
   imports = [ dank-material-shell.nixosModules.greeter ];
 
   programs.dankMaterialShell.greeter = {
     enable = true;
     compositor.name = "hyprland";
+    configHome = "/home/${myvars.username}";
+
+    quickshell.package = quickshellPkg;
   };
 }

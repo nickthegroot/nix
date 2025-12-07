a05a4d62f561c7a04eb7704b9fe82bb9f53826a7
Nick DeGroot <nick@nickthegroot.com>
2025-12-06 16:11:21 -0800

    okabe-rintarou: switch dp monitors

--- hosts/okabe-rintarou/home/displays.nix
+++ hosts/okabe-rintarou/home/displays.nix
@@ -1,19 +1,25 @@
 {
+  # BIOS defaults to DP-2 as primary (e.g. grub)
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-1,highrr,auto,1"
-        "DP-2,highrr,auto-left,1,transform,1"
+        "DP-1,highrr,auto,1,transform,1"
+        "DP-2,highrr,auto-right,1"
+      ];
+
+      exec-once = [
+        # Set cursor to DP-2 (primary) on startup
+        "hyprctl dispatch workspace 1"
       ];
 
       workspace =
         (
-          # 1-5 on DP-1
-          builtins.genList (i: "${toString (i + 1)}, monitor:DP-1") 5
+          # 1-5 on DP-2
+          builtins.genList (i: "${toString (i + 1)}, monitor:DP-2") 5
         )
         ++ (
-          # 6-9 on DP-2
-          builtins.genList (i: "${toString (i + 6)}, monitor:DP-2") 4
+          # 6-9 on DP-1
+          builtins.genList (i: "${toString (i + 6)}, monitor:DP-1") 4
         );
     };
   };

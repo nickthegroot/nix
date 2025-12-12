8213c06941b6d4fc82b4f1d5d55c6f140da4548a
Nick DeGroot <nick@nickthegroot.com>
2025-12-11 22:46:56 -0800

    steam: gamescope capSysNice

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -1,9 +1,17 @@
 {
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true; # was having minor issues without this
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
+  programs = {
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+
+    # Helpful for SteamVR
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
   };
 }

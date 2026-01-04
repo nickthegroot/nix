3dd9a10cb269dd27144b3dc86b3d47af80964c6b
Nick DeGroot <nick@nickthegroot.com>
2026-01-03 22:45:49 -0800

    fcitx5: fix catppucccin theme & modify hotkey

--- home/linux/core/language-input.nix
+++ home/linux/core/language-input.nix
@@ -1,13 +1,5 @@
 { pkgs, ... }:
 {
-  catppuccin.fcitx5 = {
-    enable = true;
-    # Currently causing issues with catpuccin config:
-    # > Error installing file '.config/fcitx5/conf/classicui.conf' outside $HOME
-    # Instead, just manually apply
-    apply = false;
-  };
-
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
@@ -19,15 +11,21 @@
         fcitx5-gtk # im module
       ];
 
-      settings.inputMethod = {
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "mozc";
+      settings = {
+        globalOptions."Hotkey/TriggerKeys" = {
+          "0" = "Super+grave";
         };
-        "Groups/0/Items/0".Name = "keyboard-us";
-        "Groups/0/Items/1".Name = "mozc";
+
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "mozc";
+        };
       };
     };
   };

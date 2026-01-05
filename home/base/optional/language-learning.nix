8fb7b77be9e0a1e29d4d36d3a1e9722dec3013c1
Nick DeGroot <nick@nickthegroot.com>
2026-01-05 00:59:28 -0800

    anki: link to local sync keyFile

--- home/base/optional/language-learning.nix
+++ home/base/optional/language-learning.nix
@@ -1,4 +1,8 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.language-learning;
 in
@@ -10,7 +14,13 @@ in
   config = lib.mkIf cfg.enable {
     programs.anki = {
       enable = true;
-      sync.url = "http://anki-sync.worldline.local";
+      sync = {
+        url = "http://anki-sync.worldline.local";
+        username = "nickthegroot";
+        # TODO: use agenix to manage secrets
+        # For now, just create the key file manually
+        keyFile = "${config.xdg.configHome}/anki/key.txt";
+      };
     };
 
     programs.brave.extensions = [

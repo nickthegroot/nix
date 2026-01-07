799881fa5818b038568df2ff97357917a1106474
Nick DeGroot <nick@nickthegroot.com>
2026-01-07 15:20:07 -0800

    nvim: add ts-error-translator

--- home/base/tui/neovim/plugins/lang/typescript.nix
+++ home/base/tui/neovim/plugins/lang/typescript.nix
@@ -63,5 +63,20 @@
 
       ts-autotag.enable = true;
     };
+
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "ts-error-translator.nvim";
+        src = pkgs.fetchFromGitHub {
+          owner = "dmmulroy";
+          repo = "ts-error-translator.nvim";
+          rev = "558abff11b9e8f4cefc0de09df780c56841c7a4b";
+          hash = "sha256-kjZwfvb0B7GC4dBBSdgC/zRmCUCfCm4H5J+8SFzANJ4=";
+        };
+      })
+    ];
+    extraConfigLua = ''
+      require("ts-error-translator").setup({ auto_attach = true })
+    '';
   };
 }

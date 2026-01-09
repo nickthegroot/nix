3491203de35d3f64680551aca4fd80bacc6e85b4
Nick DeGroot <nick@nickthegroot.com>
2026-01-09 11:46:53 -0800

    code: more keybinds

--- home/base/gui/vscode.nix
+++ home/base/gui/vscode.nix
@@ -111,6 +111,195 @@
           "<C-h>" = false;
           "<C-l>" = false;
         };
+
+        "vim.normalModeKeyBindings" = [
+          # Better up/down
+          {
+            "before" = [ "j" ];
+            "after" = [ "g" "j" ];
+          }
+          {
+            "before" = [ "k" ];
+            "after" = [ "g" "k" ];
+          }
+          # Join lines keeping cursor in place
+          {
+            "before" = [ "J" ];
+            "commands" = [
+              "editor.action.joinLines"
+            ];
+          }
+          # Clear search with ESC
+          {
+            "before" = [ "<Esc>" ];
+            "commands" = [ ":nohl" ];
+          }
+          # Copy to system clipboard
+          {
+            "before" = [ "<leader>" "y" ];
+            "after" = [ "\"" "+" "y" ];
+          }
+          # Delete to void register
+          {
+            "before" = [ "<leader>" "D" ];
+            "after" = [ "\"" "_" "d" ];
+          }
+          # Yank file paths
+          {
+            "before" = [ "<leader>" "Y" "n" ];
+            "commands" = [ "workbench.action.files.copyRelativePath" ];
+          }
+          {
+            "before" = [ "<leader>" "Y" "f" ];
+            "commands" = [ "workbench.action.files.copyRelativePath" ];
+          }
+          {
+            "before" = [ "<leader>" "Y" "F" ];
+            "commands" = [ "workbench.action.files.copyPath" ];
+          }
+          # Go to definition
+          {
+            "before" = [ "g" "d" ];
+            "commands" = [ "editor.action.revealDefinition" ];
+          }
+          # Go to references
+          {
+            "before" = [ "g" "r" ];
+            "commands" = [ "editor.action.goToReferences" ];
+          }
+          # Hover
+          {
+            "before" = [ "K" ];
+            "commands" = [ "editor.action.showHover" ];
+          }
+          # Hover Diagnostics
+          {
+            "before" = [ "T" ];
+            "commands" = [ "editor.action.peekProblem" ];
+          }
+          # Code action
+          {
+            "before" = [ "<leader>" "c" "a" ];
+            "commands" = [ "editor.action.quickFix" ];
+          }
+          # Rename Symbol
+          {
+            "before" = [ "<leader>" "c" "r" ];
+            "commands" = [ "editor.action.rename" ];
+          }
+          # Rename File
+          {
+            "before" = [ "<leader>" "c" "R" ];
+            "commands" = [ "fileutils.renameFile" ];
+          }
+          # Buffer navigation
+          {
+            "before" = [ "L" ];
+            "commands" = [ "workbench.action.nextEditor" ];
+          }
+          {
+            "before" = [ "H" ];
+            "commands" = [ "workbench.action.previousEditor" ];
+          }
+          # Next/Previous reference
+          {
+            "before" = [ "]" "]" ];
+            "commands" = [ "editor.action.wordHighlight.next" ];
+          }
+          {
+            "before" = [ "[" "[" ];
+            "commands" = [ "editor.action.wordHighlight.prev" ];
+          }
+          # Diagnostics
+          {
+            "before" = [ "]" "d" ];
+            "commands" = [ "editor.action.marker.nextInFiles" ];
+          }
+          {
+            "before" = [ "[" "d" ];
+            "commands" = [ "editor.action.marker.prevInFiles" ];
+          }
+          # Git hunks
+          {
+            "before" = [ "]" "h" ];
+            "commands" = [ "workbench.action.editor.nextChange" ];
+          }
+          {
+            "before" = [ "[" "h" ];
+            "commands" = [ "workbench.action.editor.previousChange" ];
+          }
+          # File Explorer
+          {
+            "before" = [ "<leader>" "e" ];
+            "commands" = [ "workbench.files.action.showActiveFileInExplorer" ];
+          }
+          # Fuzzy Finder
+          {
+            "before" = [ "<leader>" " " ];
+            "commands" = [ "workbench.action.quickOpen" ];
+          }
+          # Search commands
+          {
+            "before" = [ "<leader>" "s" "g" ];
+            "commands" = [ "workbench.action.findInFiles" ];
+          }
+          {
+            "before" = [ "<leader>" "s" "c" ];
+            "commands" = [ "workbench.scm.focus" ];
+          }
+          # Git commands
+          {
+            "before" = [ "<leader>" "g" "g" ];
+            "commands" = [ "workbench.view.scm" ];
+          }
+          # Format
+          {
+            "before" = [ "<leader>" "c" "f" ];
+            "commands" = [ "editor.action.formatDocument" ];
+          }
+          # Toggle Terminal
+          {
+            "before" = [ "<leader>" "t" "t" ];
+            "commands" = [ "workbench.action.terminal.toggleTerminal" ];
+          }
+        ];
+
+        "vim.visualModeKeyBindings" = [
+          # Better indenting
+          {
+            "before" = [ "<" ];
+            "after" = [ "<" "g" "v" ];
+          }
+          {
+            "before" = [ ">" ];
+            "after" = [ ">" "g" "v" ];
+          }
+          # Paste without losing register
+          {
+            "before" = [ "p" ];
+            "after" = [ "\"" "_" "d" "P" ];
+          }
+          # Copy to system clipboard
+          {
+            "before" = [ "<leader>" "y" ];
+            "after" = [ "\"" "+" "y" ];
+          }
+          # Delete to void register
+          {
+            "before" = [ "<leader>" "D" ];
+            "after" = [ "\"" "_" "d" ];
+          }
+          # Format Selection
+          {
+            "before" = [ "<leader>" "c" "f" ];
+            "commands" = [ "editor.action.formatSelection" ];
+          }
+          # Code action
+          {
+            "before" = [ "<leader>" "c" "a" ];
+            "commands" = [ "editor.action.quickFix" ];
+          }
+        ];
       };
 
       keybindings = [
@@ -130,227 +319,6 @@
         {
           key = "ctrl+l";
           command = "workbench.action.navigateRight";
-        }
-
-        # Buffer navigation (Shift+H/L)
-        {
-          key = "shift+l";
-          command = "workbench.action.nextEditor";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "shift+h";
-          command = "workbench.action.previousEditor";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # File Explorer (<leader>e)
-        {
-          key = "space e";
-          command = "workbench.files.action.showActiveFileInExplorer";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Fuzzy Finder (<leader><space>)
-        {
-          key = "space space";
-          command = "workbench.action.quickOpen";
-          when = "vim.mode == 'Normal' || vim.mode == 'Visual'";
-        }
-
-        # Search commands (<leader>s*)
-        {
-          key = "space s g";
-          command = "workbench.action.findInFiles";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space s c";
-          command = "workbench.scm.focus";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space s w";
-          command = "editor.action.addSelectionToNextFindMatch";
-          when = "vim.mode == 'Normal' || vim.mode == 'Visual'";
-        }
-        {
-          key = "space s s";
-          command = "workbench.action.gotoSymbol";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space s shift+s";
-          command = "workbench.action.showAllSymbols";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space s d";
-          command = "workbench.actions.view.problems";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space s shift+d";
-          command = "workbench.panel.markers.view.focus";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Git commands (<leader>g*)
-        {
-          key = "space g g";
-          command = "workbench.view.scm";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space g o";
-          command = "gitlens.openFileOnRemote";
-          when = "vim.mode == 'Normal' || vim.mode == 'Visual'";
-        }
-        {
-          key = "space g y";
-          command = "gitlens.copyRemoteFileUrlToClipboard";
-          when = "vim.mode == 'Normal' || vim.mode == 'Visual'";
-        }
-
-        # LSP commands (<leader>c*)
-        {
-          key = "space c l";
-          command = "workbench.action.showRuntimeExtensions";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space c a";
-          command = "editor.action.quickFix";
-          when = "vim.mode == 'Normal' || vim.mode == 'Visual'";
-        }
-        {
-          key = "space c r";
-          command = "editor.action.rename";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space c shift+r";
-          command = "fileutils.renameFile";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space c f";
-          command = "editor.action.formatDocument";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space c f";
-          command = "editor.action.formatSelection";
-          when = "vim.mode == 'Visual'";
-        }
-        {
-          key = "space c o";
-          command = "editor.action.organizeImports";
-          when = "vim.mode == 'Normal' || vim.mode == 'Visual'";
-        }
-        {
-          key = "space c v";
-          command = "python.setInterpreter";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Toggle commands (<leader>t*)
-        {
-          key = "space t i";
-          command = "editor.action.toggleInlayHints";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space t f";
-          command = "editor.action.formatDocument.none";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space t shift+f";
-          command = "editor.action.formatDocument.none";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space t t";
-          command = "workbench.action.terminal.toggleTerminal";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Go to commands (g*)
-        {
-          key = "g d";
-          command = "editor.action.revealDefinition";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "g r";
-          command = "editor.action.goToReferences";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Hover (K and T)
-        {
-          key = "shift+k";
-          command = "editor.action.showHover";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "shift+t";
-          command = "editor.action.showHover";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Next/Previous reference ([[ and ]])
-        {
-          key = "] ]";
-          command = "editor.action.wordHighlight.next";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "[ [";
-          command = "editor.action.wordHighlight.prev";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Diagnostics (]d and [d)
-        {
-          key = "] d";
-          command = "editor.action.marker.nextInFiles";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "[ d";
-          command = "editor.action.marker.prevInFiles";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Git hunks (]h and [h)
-        {
-          key = "] h";
-          command = "workbench.action.editor.nextChange";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "[ h";
-          command = "workbench.action.editor.previousChange";
-          when = "vim.mode == 'Normal'";
-        }
-
-        # Yank commands (<leader>Y*)
-        {
-          key = "space shift+y n";
-          command = "copyRelativeFilePath";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space shift+y f";
-          command = "copyRelativeFilePath";
-          when = "vim.mode == 'Normal'";
-        }
-        {
-          key = "space shift+y shift+f";
-          command = "copyFilePath";
-          when = "vim.mode == 'Normal'";
         }
 
         # File explorer operations (when focus is on explorer)

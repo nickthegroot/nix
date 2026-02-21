1cc4772e2a44fd96ba1e0eb666d7ddc33f27cd90
Nick DeGroot <nick@nickthegroot.com>
2026-02-21 15:59:34 -0800

    remove dms calc plugin (now built-in), fix yazi warning

--- modules/nixos/dms.nix
+++ modules/nixos/dms.nix
@@ -17,18 +17,6 @@
     enable = true;
     quickshell.package = quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
-    plugins = {
-      Calculator = {
-        enable = true;
-        src = pkgs.fetchFromGitHub {
-          owner = "rochacbruno";
-          repo = "DankCalculator";
-          rev = "de6dbd59b7630e897a50e107f704c1cd4a131128";
-          sha256 = "sha256-Vq+E2F2Ym5JdzjpCusRMDXd6uuAhhjAehyD/tO3omdY=";
-        };
-      };
-    };
-
     # Disable unneeded modules to save resources
     enableDynamicTheming = false;
     enableCalendarEvents = false;

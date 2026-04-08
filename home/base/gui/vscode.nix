e23261aaca50b9ded24ee0141a005a9f0269ae62
Nick DeGroot <nick@nickthegroot.com>
2026-04-08 12:27:01 -0700

    vscode: allow itself to upgrade extensions
    Previously was forcefully disabling Copilot unless updated to latest,
    which could take awhile to flow through nix pipeline.

--- home/base/gui/vscode.nix
+++ home/base/gui/vscode.nix
@@ -8,10 +8,11 @@
   programs.vscode = {
     enable = true;
 
+    mutableExtensionsDir = true;
+
     profiles.default = {
       enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
-
+      enableExtensionUpdateCheck = true;
       extensions = with pkgs.vscode-extensions; [
         vscodevim.vim
         ms-python.python

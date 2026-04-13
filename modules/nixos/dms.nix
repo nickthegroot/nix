fa56f8947ac8102272f64c0a0faec3f15a04724e
Nick DeGroot <nick@nickthegroot.com>
2026-04-12 18:00:01 -0700

    autologin on linux
    initial auth taken care of by full disk encryption

--- modules/nixos/dms.nix
+++ modules/nixos/dms.nix
@@ -1,18 +1,9 @@
 {
-  myvars,
   quickshell,
   pkgs,
   ...
 }:
 {
-  services.displayManager.dms-greeter = {
-    enable = true;
-    quickshell.package = quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
-
-    compositor.name = "hyprland";
-    configHome = "/home/${myvars.username}";
-  };
-
   programs.dms-shell = {
     enable = true;
     quickshell.package = quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,5 +1,17 @@
+{ myvars, pkgs, ... }:
 {
   programs.hyprland.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/start-hyprland";
+        user = myvars.username;
+      };
+      default_session = initial_session;
+    };
+  };
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
